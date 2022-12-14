bb484abd37c4542a7b6c0d419f65e1875082e7d0
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2022-12-14 15:02:34 +0530

    Initial commit Pigihi

---
+++ src/main/java/com/pigihi/PigihiAuthNAuthZApplication.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigihiAuthNAuthZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigihiAuthNAuthZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pigihi/PigihiAuthNAuthZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigihiAuthNAuthZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
